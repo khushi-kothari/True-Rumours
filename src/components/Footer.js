0a853b3cd4713d303ed61511ac5006fea3cf6e8e
Khushi Kothari <62203565+khushi-kothari@users.noreply.github.com>
2024-01-03 23:21:52 +0530

    Updating the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
+      <div class="container px-5 py-24">
         <div class="flex flex-row md:text-left text-center order-first">
           <div class="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
@@ -11,35 +11,18 @@ function Footer() {
             </h2>
             <nav class="list-none mb-10">
               <li>
-                <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                <a class="text-gray-600 hover:text-gray-800">Entertainment</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                <a class="text-gray-600 hover:text-gray-800">
+                  Health & Fitness
+                </a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                <a class="text-gray-600 hover:text-gray-800">Marketplace</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-              </li>
-            </nav>
-          </div>
-          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
-            </h2>
-            <nav class="list-none mb-10">
-              <li>
-                <a class="text-gray-600 hover:text-gray-800">First Link</a>
-              </li>
-              <li>
-                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-              </li>
-              <li>
-                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                <a class="text-gray-600 hover:text-gray-800">Culture</a>
               </li>
             </nav>
           </div>
@@ -49,16 +32,35 @@ function Footer() {
             </h2>
             <nav class="list-none mb-10">
               <li>
-                <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                <a class="text-gray-600 hover:text-gray-800">Games & Events</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                <a class="text-gray-600 hover:text-gray-800">Investments</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                <a class="text-gray-600 hover:text-gray-800">Small Business</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                <a class="text-gray-600 hover:text-gray-800">Teens</a>
+              </li>
+            </nav>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              CATEGORIES
+            </h2>
+            <nav class="list-none mb-10">
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Sunday Styles</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">The Millennium</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Times Topics</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Women's Health</a>
               </li>
             </nav>
           </div>
@@ -72,23 +74,25 @@ function Footer() {
                   for="footer-field"
                   class="leading-7 text-sm text-gray-600"
                 >
-                  Placeholder
+                  to TrueRumours{" "}
                 </label>
-                <input
-                  type="text"
-                  id="footer-field"
-                  name="footer-field"
-                  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
+                <div className="md:flex justify-center items-center">
+                  <input
+                    type="text"
+                    id="footer-field"
+                    name="footer-field"
+                    class="w-full mr-2 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                  <button class="mt-2 md:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-1 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                    Button
+                  </button>
+                </div>
               </div>
-              <button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
-              </button>
             </div>
             <p class="text-gray-500 text-sm mt-2 md:text-left text-center">
-              Bitters chicharrones fanny pack
+              Stay updated on latest news
               <br class="lg:block hidden" />
-              waistcoat green juice
+              around the globe.
             </p>
           </div>
         </div>
@@ -108,18 +112,18 @@ function Footer() {
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span class="ml-3 text-xl">Tailblocks</span>
+            <span class="ml-3 text-xl">TrueRumours</span>
           </a>
           <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-            © 2020 Tailblocks —
-            <a
+            © 2024 TrueRumours
+            {/* <a
               href="https://twitter.com/knyttneve"
               rel="noopener noreferrer"
               class="text-gray-600 ml-1"
               target="_blank"
             >
               @knyttneve
-            </a>
+            </a> */}
           </p>
           <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
             <a class="text-gray-500">
